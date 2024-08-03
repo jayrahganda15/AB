82311d24460f56a4295d930a4b7d0672dcd6f72a
jayrahganda15 <josepogj19@gmail.com>
2024-08-03 14:15:39 +0800

    Create randomreact.js

---
+++ script/event/randomreact.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+
+module.exports.config = {
+  name: "randomReact",
+  version: "69",
+  credits: "cliff",
+};
+
+module.exports.handleEvent = async function ({ api, event }) {
+  if (event.body) {
+    const emojis = ['🌟', '🤖', '🔥', '🐶', '😛', '❤️', '😆'];
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+
+    api.setMessageReaction(randomEmoji, event.messageID, () => {}, true);
+  }
+};
